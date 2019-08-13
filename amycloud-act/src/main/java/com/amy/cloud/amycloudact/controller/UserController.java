98b9a40fbd6620094793142105109737287fb107
yanjie <17610575821@163.com>
2019-08-13 14:54:42 +0800

    add config server

--- amycloud-act/src/main/java/com/amy/cloud/amycloudact/controller/UserController.java
+++ amycloud-act/src/main/java/com/amy/cloud/amycloudact/controller/UserController.java
@@ -1,12 +1,16 @@
-//package com.amy.cloud.amycloudact.controller;
-//
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * @Author: yanjie
-// * @Description:
-// * @Date: 2019/08/12 19:04
-// */
-//@RestController
-//public class UserController {
-//}
+package com.amy.cloud.amycloudact.controller;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: yanjie
+ * @Description:
+ * @Date: 2019/08/12 19:04
+ */
+@RestController
+public class UserController {
+
+}
--- amycloud-eureka/src/main/java/com/amy/cloud/amycloudeureka/AmycloudEurekaApplication.java
+++ amycloud-eureka/src/main/java/com/amy/cloud/amycloudeureka/AmycloudEurekaApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-@EnableEurekaServer
+@EnableEurekaServer //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
 public class AmycloudEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmycloudEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AmycloudEurekaApplication.class, args);
+    }
 
 }
