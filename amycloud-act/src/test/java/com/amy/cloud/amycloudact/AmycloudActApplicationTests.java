7c6118c58ff97cea42e1477c8c8fdbe0debf43f3
yanjie <amyyanjie@gmail.com>
2019-09-05 17:53:41 +0800

    zuul指定routes

--- amycloud-act/src/test/java/com/amy/cloud/amycloudact/AmycloudActApplicationTests.java
+++ amycloud-act/src/test/java/com/amy/cloud/amycloudact/AmycloudActApplicationTests.java
@@ -17,58 +17,30 @@ public class AmycloudActApplicationTests {
     //1000021
 
     public static void main(String[] args) {
-        int x = 8; //2147483647
-        investAmount("500.24");
-        investAmount1("500.24");
-        System.out.println();
+        int[] nums = {1, 3, 5, 7}; //4  2  [0,3]
+        int target = 8;
+        System.out.println(searchInsert(nums,target));
     }
 
 
-    private static void investAmount(String amount){
-        boolean ISPASS=true;
-        String msg;
-        BigDecimal[] result =  new BigDecimal(amount).divideAndRemainder(new BigDecimal(100));
-        if (!"".equals(result[1]) && result[1] != null) {
-            if(result[1].compareTo(BigDecimal.ZERO)!=0){
-                ISPASS=Boolean.FALSE;
-                msg="购买金额必须为100整数倍！";
-                System.out.println(result[0]);
-                System.out.println(result[1]);
-                System.out.println(msg);
 
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) { //注意循环条件
+            mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1; //left右移
+            } else {
+                right = right - 1; //right左移
             }
-            System.out.println("======");
         }
-        /*if( amt.intValue() != 0){
-            ISPASS=Boolean.FALSE;
-            MESSAGE="购买金额必须为100整数倍！";
-        }*/
+        return left; //若target不存在于nums数组，返回插入位置：left
     }
 
-    private static void investAmount1(String amount){
-        BigDecimal amt =  new BigDecimal(amount).remainder(new BigDecimal(100));
-        System.out.println(amt);
-        System.out.println("intValue:");
-        System.out.println (amt.intValue());
-        if( amt.compareTo(BigDecimal.ZERO)!=0){
-           String msg="购买金额必须为100整数倍！";
-            System.out.println(msg);
-        }
-    }
 
-    //	给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
-    public int reverse(int x) {
-        boolean a = x >= 0;
-        x = Math.abs(x);
-        int result = 0;
-        int limit = Integer.MAX_VALUE;
-        while (x > 0) { //注意这里是>0，并非之前的!=0;
-            if (result > (limit / 10)) {
-                return 0;
-            }
-            result = result * 10 + x % 10;
-            x = x / 10;
-        }
-        return a ? result : 0 - result;
-    }
+
 }
