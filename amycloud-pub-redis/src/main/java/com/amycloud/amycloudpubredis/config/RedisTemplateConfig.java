f21958b9a9c4e33e175091acde8650873fa52fef
yanjie <amyyanjie@gmail.com>
2019-09-21 15:33:03 +0800

    redis Module

---
+++ amycloud-pub-redis/src/main/java/com/amycloud/amycloudpubredis/config/RedisTemplateConfig.java
@@ -0,0 +1,9 @@
+package com.amycloud.amycloudpubredis.config;
+
+/**
+ * @Author: yanjie
+ * @Description:
+ * @Date: 2019/09/21 15:30
+ */
+public class RedisTemplateConfig {
+}
---
+++ amycloud-pub-redis/src/test/java/com/amycloud/amycloudpubredis/AmycloudPubRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amycloud.amycloudpubredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmycloudPubRedisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
