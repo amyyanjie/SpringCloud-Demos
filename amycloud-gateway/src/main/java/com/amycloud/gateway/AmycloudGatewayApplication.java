572b20f44032e5a7bdc81499727c0d5e516f59b8
yanjie <amyyanjie@gmail.com>
2019-09-29 10:32:42 +0800

    zuul不向eureka注册，使用gateway

--- amycloud-gateway/src/main/java/com/amycloud/gateway/AmycloudGatewayApplication.java
+++ amycloud-gateway/src/main/java/com/amycloud/gateway/AmycloudGatewayApplication.java
@@ -1,9 +1,9 @@
-package com.amycloud.amycloudgateway;
+package com.amycloud.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+@SpringCloudApplication
 public class AmycloudGatewayApplication {
 
     public static void main(String[] args) {
--- amycloud-gateway/src/test/java/com/amycloud/gateway/AmycloudGatewayApplicationTests.java
+++ amycloud-gateway/src/test/java/com/amycloud/gateway/AmycloudGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amycloud.amycloudgateway;
+package com.amycloud.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
