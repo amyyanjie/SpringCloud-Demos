03c68ab98b0fd195357f2ba6aecb7fdb17ebd4e7
yanjie <amyyanjie@gmail.com>
2019-09-29 10:28:46 +0800

    add module gateway

---
+++ amycloud-gateway/src/main/java/com/amycloud/amycloudgateway/AmycloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.amycloud.amycloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmycloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmycloudGatewayApplication.class, args);
+    }
+
+}
